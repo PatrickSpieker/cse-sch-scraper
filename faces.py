27adbc2ed33ccb92f016850f03f77125626ab15a
patrick <patrickspieker@users.noreply.github.com>
2015-07-21 23:59:49 -0700

    Practice with multileveling tree

--- faces.py
+++ faces.py
@@ -1,8 +1,14 @@
 from ete2 import Tree, TreeNode
 
 list = ["CSE 142", "CSE 143", "CSE 160"]
+list_child = ["CSE 311", "CSE 312"]
+
 classes = Tree()
 for i in list:
     j = TreeNode(name=i)
     classes.add_child(child=j)
+    for k in list_child:
+        l = TreeNode(name=k)
+        j.add_child(child=l)
+        
 classes.show()

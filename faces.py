b0cb4e036a89719869630f59154fcaa554425eaa
patrick <patrickspieker@users.noreply.github.com>
2015-07-22 00:32:08 -0700

    Working concept of multi-tiered labling

--- faces.py
+++ faces.py
@@ -1,14 +1,17 @@
-from ete2 import Tree, TreeNode
+from ete2 import Tree, TreeNode, TextFace
 
 list = ["CSE 142", "CSE 143", "CSE 160"]
 list_child = ["CSE 311", "CSE 312"]
 
 classes = Tree()
-for i in list:
-    j = TreeNode(name=i)
-    classes.add_child(child=j)
+for course in list:
+    parent = TreeNode(name=course)
+    face = TextFace(text=str(course))
+    parent.add_face(face, column=1)
+    classes.add_child(child=parent)
     for k in list_child:
         l = TreeNode(name=k)
-        j.add_child(child=l)
-        
+        parent.add_child(child=l)
+
+
 classes.show()
